3a0fafc90aff81e0bcc36f041b151c4a53107906
Mohammed Elshenawy <MElshenawy@healthinsightsgroup.com>
2026-05-16 03:06:45 +0300

    fix: add missing angular.json for procedures and appointments MFEs

--- emr-allergies-mfe/federation.config.js
+++ emr-allergies-mfe/federation.config.js
@@ -1,2 +1,2 @@
-﻿const { withNativeFederation, shareAll } = require('@angular-architects/native-federation/config');
+const { withNativeFederation, shareAll } = require('@angular-architects/native-federation/config');
 module.exports = withNativeFederation({ name: 'emr-allergies-mfe', exposes: { './Component': './src/app/app.component.ts' }, shared: { ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }) }, skip: ['rxjs/ajax','rxjs/fetch','rxjs/testing','rxjs/webSocket'], features: { ignoreUnusedDeps: true } });
